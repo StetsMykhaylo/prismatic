2b5277c5e11c17cc7d1c70985987f8eaa2bab9e6
StetsMykhaylo <75065477+StetsMykhaylo@users.noreply.github.com>
2021-06-26 16:47:13 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+let burgerClass = document.querySelector('.burger');
+let burger = document.querySelector('.burger_menu');
+
+function t1() {
+    burger.classList.toggle("hidden");
+}
+burgerClass.onclick = t1;
+
